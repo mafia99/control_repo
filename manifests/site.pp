e58d4fbd08d77d175bee56c96965b59698879b48
Muhammad Ali Fayyaz <muhammadalifayyaz@gmail.com>
2021-07-21 18:19:19 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,5 @@
 node default {
-  file {'/root/README':
-    ensure => file,
-    content => 'This is a readme',
-    owner => 'root',
-  }
 }
+node 'master.puppet.vm'{
+  include role::master_server
+}  
