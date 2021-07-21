e081e0f59dc907c08ef1dbf3c49372f96a80116a
Muhammad Ali Fayyaz <muhammadalifayyaz@gmail.com>
2021-07-21 22:29:47 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -17,4 +17,8 @@ class minecraft {
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
+  service {'minecraft':
+    ensure => running,
+    enable => true,
+  }
 }
